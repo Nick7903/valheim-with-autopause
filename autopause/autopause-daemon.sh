681212ef79eadb630264a4d844912edf11b740a5
Nick7903 <Nick7903@gmail.com>
2024-08-01 21:06:00 +0200

    Update autopause-daemon.sh

--- autopause/autopause-daemon.sh
+++ autopause/autopause-daemon.sh
@@ -3,9 +3,10 @@
 # reset network data counter
 nstat -n
 
-while : do
+while :; do
   sleep 60s
-  if ! nstat | awk '/UdpInDatagrams/{print $2}' ; then
+  udp_in_datagrams=$(nstat | awk '/UdpInDatagrams/{print $2}')
+  if [ -z "$udp_in_datagrams" ]; then
     /autopause/pause
   fi
 done
